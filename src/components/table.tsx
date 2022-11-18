5e64c4498a9bae34687a80757b74f19cfe0e69e9
Wasif Jameel <wasif.jameel1@gmail.com>
2022-11-18 06:50:32 +0500

    console errors fixed

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -2,8 +2,6 @@ import { useState } from 'react';
 import { Listbox } from '@headlessui/react';
 
 import {
-  FaEthereum,
-  FaRegCopy,
   FaSortDown,
   FaSortUp,
   FaSort,
@@ -17,6 +15,7 @@ const peopleDropdown = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }];
 
 const people = [
   {
+    id: 1,
     name: 'Lindsay Walton',
     title: 'Front-end Developer',
     department: 'Optimization',
@@ -26,6 +25,7 @@ const people = [
       'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
   {
+    id: 2,
     name: 'Lindsay Walton',
     title: 'Front-end Developer',
     department: 'Optimization',
@@ -35,6 +35,7 @@ const people = [
       'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
   {
+    id: 3,
     name: 'Lindsay Walton',
     title: 'Front-end Developer',
     department: 'Optimization',
@@ -44,6 +45,7 @@ const people = [
       'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
   {
+    id: 4,
     name: 'Lindsay Walton',
     title: 'Front-end Developer',
     department: 'Optimization',
@@ -53,6 +55,7 @@ const people = [
       'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
   {
+    id: 5,
     name: 'Lindsay Walton',
     title: 'Front-end Developer',
     department: 'Optimization',
@@ -66,115 +69,87 @@ const people = [
 export default function Table() {
   const [selectedPerson, setSelectedPerson] = useState(peopleDropdown[0]);
   return (
-    <div className="px-4 sm:px-6 lg:px-8">
-      <div className="flex flex-col lg:flex-row items-start lg:items-center justify-between mt-3">
-        <h1 className="text-xl font-semibold text-gray-900">
-          Immuna Monitor Portal
-        </h1>
-        <div className="flex flex-col md:flex-row items-baseline lg:items-center justify-between">
-          <input
-            className="bg-white rounded-full w-56 lg:w-72 px-4 py-2 mr-3 mt-2"
-            placeholder="Search"
-          />
-          <div className="flex mt-3">
-            <div className="bg-blue-100 rounded-full">
-              <div className="flex items-center mx-1">
-                <FaEthereum className="bg-blue-100" color="#6C6C6C" />{' '}
-                <p>ETH</p>
-                <div className="bg-blue-200 rounded-full ml-2 -mr-1 px-4 py-2">
-                  0xBAD7...E116
-                </div>
-              </div>
-            </div>
-            <div className="bg-blue-200 hover:bg-blue-100 rounded-full p-2.5 ml-2 cursor-pointer">
-              <FaRegCopy color="#6C6C6C" />
-            </div>
+    <div className="mt-8 flex flex-col">
+      <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+          <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+            <table className="min-w-full divide-y divide-gray-300">
+              <thead className="bg-blue-100">
+                <tr>
+                  <th
+                    scope="col"
+                    className="flex items-center py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
+                    Protocol
+                    <FaSort />
+                  </th>
+                  <th
+                    scope="col"
+                    className=" px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                    <div className="flex items-center">
+                      Pool
+                      <FaSortUp />
+                    </div>
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                    <div className="flex items-center">
+                      Balance
+                      <FaSortDown />
+                    </div>
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                    <div className="flex items-center">
+                      Risk
+                      <FaSort />
+                    </div>
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="divide-y divide-gray-200 bg-white">
+                {people.map((person, index) => (
+                  <tr
+                    key={person.id}
+                    className={index % 2 === 0 ? undefined : 'bg-[#F4F9FF]'}>
+                    <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
+                      <div className="flex items-center font-medium text-gray-900">
+                        Uniswap <FaExternalLinkAlt className="ml-2" size={10} />
+                      </div>
+                    </td>
+                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                      <div className="flex items-center text-gray-900">
+                        0x75f5...1666{' '}
+                        <FaExternalLinkAlt className="ml-2" size={10} />
+                      </div>
+                    </td>
+                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                      1.104008 WETH / 2,888.28 USDC
+                    </td>
+                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                      <span className="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">
+                        Active
+                      </span>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
-      <div className="mt-8 flex flex-col">
-        <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-            <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
-              <table className="min-w-full divide-y divide-gray-300">
-                <thead className="bg-blue-100">
-                  <tr>
-                    <th
-                      scope="col"
-                      className="flex items-center py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
-                      Protocol
-                      <FaSort />
-                    </th>
-                    <th
-                      scope="col"
-                      className=" px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                      <div className="flex items-center">
-                        Pool
-                        <FaSortUp />
-                      </div>
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                      <div className="flex items-center">
-                        Balance
-                        <FaSortDown />
-                      </div>
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                      <div className="flex items-center">
-                        Risk
-                        <FaSort />
-                      </div>
-                    </th>
-                  </tr>
-                </thead>
-                <tbody className="divide-y divide-gray-200 bg-white">
-                  {people.map((person, index) => (
-                    <tr
-                      key={person.email}
-                      className={index % 2 === 0 ? undefined : 'bg-[#F4F9FF]'}>
-                      <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
-                        <div className="flex items-center font-medium text-gray-900">
-                          Uniswap{' '}
-                          <FaExternalLinkAlt className="ml-2" size={10} />
-                        </div>
-                      </td>
-                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                        <div className="flex items-center text-gray-900">
-                          0x75f5...1666{' '}
-                          <FaExternalLinkAlt className="ml-2" size={10} />
-                        </div>
-                      </td>
-                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                        1.104008 WETH / 2,888.28 USDC
-                      </td>
-                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                        <span className="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">
-                          Active
-                        </span>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-          </div>
+      <div className="mt-10 flex flex-col lg:flex-row items-start lg:items-center justify-between">
+        <div className="flex items-center mb-4 ">
+          Show{' '}
+          <Dropdown
+            selectedPerson={selectedPerson}
+            setSelectedPerson={setSelectedPerson}
+          />{' '}
+          results of 50 entries
         </div>
-        <div className="mt-10 flex flex-col lg:flex-row items-start lg:items-center justify-between">
-          <div className="flex items-center mb-4 ">
-            Show{' '}
-            <Dropdown
-              selectedPerson={selectedPerson}
-              setSelectedPerson={setSelectedPerson}
-            />{' '}
-            results of 50 entries
-          </div>
-          <div className=" flex items-center mb-4 ">
-            <Pagination />
-          </div>
+        <div className=" flex items-center mb-4 ">
+          <Pagination />
         </div>
       </div>
     </div>
@@ -204,7 +179,7 @@ const Pagination = () => (
       <FaAngleLeft />
     </PageButton>
     {[...new Array(5)].map((_, item) => (
-      <PageButton>{++item}</PageButton>
+      <PageButton key={item}>{++item}</PageButton>
     ))}
     <PageButton>
       <FaAngleRight />
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,36 @@
+import { FaEthereum, FaRegCopy } from 'react-icons/fa';
+
 import { Table, Layout } from '../components';
 
 export default function Dashabord() {
   return (
     <Layout>
-      <main className="bg-[#F8F8F8] h-full">
+      <main className="bg-[#F8F8F8] h-full px-4 sm:px-6 lg:px-8">
+        <div className="flex flex-col lg:flex-row items-start lg:items-center justify-between mt-3">
+          <h1 className="text-xl font-semibold text-gray-900">
+            Immuna Monitor Portal
+          </h1>
+          <div className="flex flex-col md:flex-row items-baseline lg:items-center justify-between">
+            <input
+              className="bg-white rounded-full w-56 lg:w-72 px-4 py-2 mr-3 mt-2"
+              placeholder="Search"
+            />
+            <div className="flex mt-3">
+              <div className="bg-blue-100 rounded-full">
+                <div className="flex items-center mx-1">
+                  <FaEthereum className="bg-blue-100" color="#6C6C6C" />{' '}
+                  <p>ETH</p>
+                  <div className="bg-blue-200 rounded-full ml-2 -mr-1 px-4 py-2">
+                    0xBAD7...E116
+                  </div>
+                </div>
+              </div>
+              <div className="bg-blue-200 hover:bg-blue-100 rounded-full p-2.5 ml-2 cursor-pointer">
+                <FaRegCopy color="#6C6C6C" />
+              </div>
+            </div>
+          </div>
+        </div>
         <Table />
       </main>
     </Layout>
